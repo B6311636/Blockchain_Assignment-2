d6b2bfe50dd50088be40582b9f35b80d6ad6b5a9
Pratchaya Saenphoi <B6311636@g.sut.ac.th>
2023-02-25 01:01:14 +0700

    patch Show list Table

--- ProofOfBocchi.sol
+++ ProofOfBocchi.sol
@@ -1,33 +1,104 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
+pragma experimental ABIEncoderV2;
 
-contract ProofOfBocchi {
+contract ProofOfBocchi {  
 
-    struct BocchiMetadata {
-        string name;
-        int price;
-        uint256 timestamp;
-        address owner;
+  mapping (bytes32 => bool) private listBocchi;
+
+    struct BocchiMetadata{
+        string times;
+        string cards;
+        string owners;
+    }
+    BocchiMetadata[] bocchi;
+  //---events---
+  event NameAdded(
+    address from,   
+    string text,
+    bytes32 hash,
+    string time,
+    string owner
+
+  );
+  
+  event RegistrationError(
+    address from,
+    string text,
+    string reason
+  );
+
+  // store the proof for a student in the contract state
+  function recordProof(bytes32 proof) private {
+    listBocchi[proof] = true;
+  }
+  
+  function registration(string memory name ,string memory owner ,string memory time) public payable {
+
+    if(keccak256(bytes(name)) == keccak256(bytes("Gotoh Hitori"))) {
+        if (msg.value != 0.001 ether) {
+            //---fire the event---
+            emit RegistrationError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.001 ether");
+            //---refund back to the sender---
+            payable(msg.sender).transfer(msg.value);
+            //---exit the function---
+            return;
+        } 
+
+    }else if(keccak256(bytes(name)) == keccak256(bytes("Kita Ikuyo"))){
+        if (msg.value != 0.002 ether) {
+            emit RegistrationError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.002 ether");
+            payable(msg.sender).transfer(msg.value);
+            return;
+        }     
+
+    }else if(keccak256(bytes(name)) == keccak256(bytes("Ijichi Nijika"))){
+        if (msg.value != 0.003 ether) {
+            emit RegistrationError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.003 ether");
+            payable(msg.sender).transfer(msg.value);
+            return;
+        }     
+
+    }else if(keccak256(bytes(name)) == keccak256(bytes("Yamada Ryo"))){
+        if (msg.value != 0.004 ether) {
+            emit RegistrationError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.003 ether");
+            payable(msg.sender).transfer(msg.value);
+            return;
+        }     
+
+    }else if(keccak256(bytes(name)) == keccak256(bytes("Ikuyo SSR"))){
+        if (msg.value != 0.005 ether) {
+            emit RegistrationError(msg.sender, name, "Incorrect amount of Ether. You should pay 0.005 ether");
+            payable(msg.sender).transfer(msg.value);
+            return;
+        }
+
+    }else{
+        emit RegistrationError(msg.sender, name, "Card not found, please contact seller");
+        payable(msg.sender).transfer(msg.value);
+        return;
     }
 
-    mapping(bytes32 => BocchiMetadata) private listBocchi;
+    recordProof(hashing(name));
 
-    // store the metadata for a Bocchi in the contract state
-    function recordMetadata(string memory name, int price) public {
-        bytes32 hash = hashing(name);
-        require(listBocchi[hash].timestamp == 0, "Bocchi metadata already exists");
-        listBocchi[hash] = BocchiMetadata(name, price, block.timestamp, msg.sender);
-    }
+    bocchi.push(BocchiMetadata(time,name,owner));
 
-    // SHA256 for Integrity
-    function hashing(string memory name) private pure returns (bytes32) {
-        return sha256(bytes(name));
-    }
+    emit NameAdded(msg.sender, name, hashing(name), time, owner);  
+  }
+  
+  // SHA256 for Integrity
+  function hashing(string memory name) private 
+  pure returns (bytes32) {
+    return sha256(bytes(name));
+  }
+  
+  // check name of student in this class
+  function checkName(string memory name) public 
+  view returns (bool) {
+    return listBocchi[hashing(name)];
+  }
 
-    // Function to get the metadata of a Bocchi
-    function getBocchiMetadata(bytes32 hash) public view returns (string memory, int, uint256, address) {
-        BocchiMetadata memory metadata = listBocchi[hash];
-        require(metadata.timestamp > 0, "Bocchi metadata not found");
-        return (metadata.name, metadata.price, metadata.timestamp, metadata.owner);
-    }
+  function getBocchiMetadata() public view returns(BocchiMetadata[] memory){
+    return bocchi;
+  }
 }
